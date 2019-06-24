d033dd0c965fd06dc31906d72199cdbb41d2c821
ads00 <adess00@hotmail.fr>
2019-06-24 18:28:43 +0200

    [update] improve cx errors

--- include/nds/cx/index_of.hpp
+++ include/nds/cx/index_of.hpp
@@ -1,12 +1,27 @@
 #ifndef INCLUDE_NDS_CX_INDEX_OF_HPP_NDS
 #define INCLUDE_NDS_CX_INDEX_OF_HPP_NDS
 
+#define nds_cx_error_value(Source, Err, ...) ::nds::cx::cx_error_value<Source, Err< __VA_ARGS__ > >::value
+
 namespace nds::cx
 {
-    namespace internal
+    template<class...>
+    struct cx_err;
+
+    namespace errs
     {
-        struct cx_err_type_not_found;
-    } // internal
+        template<class...>
+        struct out_of_range;
+    } // errs
+
+    template<class Source, class Err>
+    struct cx_error_occured;
+
+    template<template<class...> class Source, class Err>
+    struct cx_error_occured_tpl;
+
+    template<class Source, class Err>
+    struct cx_error_value { static constexpr auto value = cx_error_occured<Source, Err>{}; };
 
     template<class T, class Container>
     struct index_of;
@@ -14,7 +29,7 @@ namespace nds::cx
     template<class T, template<class...> class Container>
     struct index_of<T, Container<>>
     {
-        constexpr static auto value = internal::cx_err_type_not_found;
+        constexpr static auto value = nds_cx_error_value(index_of, errs::out_of_range);
     };
 
     template<class T, template<class...> class Container, class... Ts>
